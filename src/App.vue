756f4062581cf4aed6d6a7abb655e9d86d93615f
yann-catlinn <y.catrilelbun@alumnos.duoc.cl>
2020-10-04 09:35:58 -0300

    vue basic structure

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Hola en vue',
+      };
+    }
+  }
+</script>
+
+<style scoped>
+  #app {
+    font-size: 25px;
+    font-family: "Calibri", sans-serif;
+    color: darkblue;
+    background: lightblue;
+  }
+</style>
